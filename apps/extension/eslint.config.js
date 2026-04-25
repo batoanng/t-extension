dccea9b268042354cd5029a47aff2f50108c6493
Ba Toan Nguyen <michael.batoan.nguyen@gmail.com>
2026-04-25 23:28:35 +1000

    Add validation github workflow

--- apps/extension/eslint.config.js
+++ apps/extension/eslint.config.js
@@ -1,8 +1,12 @@
+import { fileURLToPath } from 'node:url';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+
+const tsconfigRootDir = fileURLToPath(new URL('.', import.meta.url));
 
 export default tseslint.config(
   {
@@ -14,6 +18,10 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
